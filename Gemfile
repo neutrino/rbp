1e1a9590beb5107405e960ef0eec5a215fcb2714
ranendra <ranendr@sprout-technology.com>
2011-05-30 08:50:14 +0545

    initial commit, deleted capybara html files

--- Gemfile
+++ Gemfile
@@ -1,14 +1,17 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.1'
+gem 'rails', '3.0.6'
 gem 'authlogic'
 gem 'mysql', '2.8.1'
+gem 'rake','0.8.7'
 
-group :test do
-  gem 'cucumber-rails'
-  gem 'capybara'
-  gem 'rspec-rails', '2.0.0.beta.6'
-  gem 'launchy'
-end
-  
+
+gem 'cucumber-rails'
+gem 'capybara'
+gem 'rspec-rails'
+gem 'launchy'
+
 gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'jquery-rails'
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,20 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  validates_length_of :text, :minimum => 4
+
+  def commenter_address
+    "#{self.user.address.city},#{self.user.address.country}"
+  end
+
+  def self.recent(count)
+    order("created_at DESC").limit(count)
+  end
+
+  def is_minimum_length?
+    if self.text.length < 4
+      return false
+    else
+      return true
+    end
+  end
 end
+
