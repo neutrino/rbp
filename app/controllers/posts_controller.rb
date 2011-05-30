bfe3a72043ef893d1d1533715d28435803552bc0
ranendra <ranendr@sprout-technology.com>
2011-05-30 09:20:22 +0545

    added validations in post model for title and text, refactored the controller to use respond_with methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,29 @@
 class PostsController < ApplicationController
 
   before_filter :login_required, :except => [:index, :show]
+  respond_to :html, :xml
 
   def index
     @posts = Post.all
-    # show all posts that have more than 10 comments
     @popular_posts = Post.popular_posts
-
-    respond_to do |format|
-      format.html
-      format.xml
-    end
+    respond_with(@popular_posts, @posts)
   end
 
   def new
-    @post = Post.new
-
-    respond_to do |format|
-      format.html
-      format.xml
-    end
+    @post = current_user.posts.new
+    respond_with(@post)
   end
 
   def create
-    @post = Post.new(params[:post])
-
-    if @post.title.present? && @post.text.present? && @post.save
-      flash[:notice] = "Post has been created."
-      redirect_to user_post_path(current_user,@post)
-    else
-      flash[:notice] = "Post has not been created."
-      render :action => "new"
-    end
+    @post = current_user.posts.new(params[:post])
+    flash[:notice] = @post.save ?  "Post has been created." : "Post has not been created."
+    respond_with(@post)
   end
 
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-
-    respond_to do |format|
-      format.html
-      format.xml
-    end
+    respond_with(@post, @comment)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  #validations
+  validate :title, :presence => true
+  validate :text, :presence => true
+
   def recent_commenters
     self.comments.recent(5).collect{|comment| comment.user.login}.map{|commenter| "<li>#{commenter}</li>"}.join("")
   end
 
+
   def popular_posts
+    # return posts that have more than 10 comments
     self.all.include(:comments).collect{|p| p if p.comments.length > NO_OF_COMMENTS}.compact
   end
 
