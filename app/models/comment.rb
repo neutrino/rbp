248955074c4b530458ed534f9dd20cdaa715c494
ranendra <ranendr@sprout-technology.com>
2011-05-30 09:43:19 +0545

    added scope for recent comments and before_create callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,20 +1,18 @@
 class Comment < ActiveRecord::Base
+
   belongs_to :user
+
+  before_create :is_minimum_length
+
+  scope :recent, lambda {|count = nil | order("created_at DESC").limit(count)}
 
   def commenter_address
     "#{self.user.address.city},#{self.user.address.country}"
   end
 
-  def self.recent(count)
-    order("created_at DESC").limit(count)
+  def is_minimum_length?
+    return true if self.text.length > 3
   end
 
-  def is_minimum_length?
-    if self.text.length < 4
-      return false
-    else
-      return true
-    end
-  end
 end
 
